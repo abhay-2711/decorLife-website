f409e6df11ec21c074ec633c08205a3ffb85effd
Abhay <abhaypratap.s20@iiits.in>
2023-03-13 19:02:39 +0530

    server1

--- Server/server.js
+++ Server/server.js
@@ -4,43 +4,48 @@ const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
 
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: ["https://decor-life.netlify.app","http://localhost:3000"],
+  })
+);
 
 //Handling Uncaught Exception
-process.on("uncaughtException",(err)=>{
-    console.log(`Error:${err.message}`);
-    console.log(`Shutting down the server due to Uncaught Exception`);
+process.on("uncaughtException", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
 
-    process.exit(1);
-})
+  process.exit(1);
+});
 
-
-//config 
-dotenv.config({path:"config/config.env"});
+//config
+dotenv.config({ path: "config/config.env" });
 
 //connecting to database
-connectDatabase()
+connectDatabase();
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // const server = httpServer.listen(process.env.PORT,()=>{
 //     console.log("Server is working on : ", process.env.PORT);
 // })
 
-const server = app.listen(process.env.PORT,()=>{
-    console.log(`Server is working on http://localhost:${process.env.PORT}`)
-})
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
 
 //Unhandled Promise Rejection
-process.on("unhandledRejection",(err)=>{
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
-    server.close(()=>{
-        process.exit(1);
-    })
-})
+  server.close(() => {
+    process.exit(1);
+  });
+});
